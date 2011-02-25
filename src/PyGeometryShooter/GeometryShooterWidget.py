a44e65b22253b9a52256dbf2d6d70e0e33492207
johnDonson <janwasak@gmail.com>
2011-02-25 13:14:16 +0100

    commit

--- src/PyGeometryShooter/GeometryShooterWidget.py
+++ src/PyGeometryShooter/GeometryShooterWidget.py
@@ -5,10 +5,11 @@ Created on 25-02-2011
 '''
 import math
 
+# import OpenGL.GL
 from OpenGL.GL import *
+# import OpenGL
 from OpenGL.GLU import *
-from PyQt4 import QtGui
-from PyQt4.QtOpenGL import *
+from PyQt4.QtOpenGL import QGLWidget
 
 class GeometryShooterWidget(QGLWidget):
     def __init__(self, parent):
